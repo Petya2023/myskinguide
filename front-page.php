91351f69ea4ea82d4b59d22e4a92fb6dbc54e0b6
Petya Naydenova <petyanayde@gmail.com>
2024-09-17 01:50:22 +0300

    remove carousel folder

--- front-page.php
+++ front-page.php
@@ -42,32 +42,6 @@ get_header();
         </div>
     </div>
 </section>
-<!-- <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="<?php echo get_theme_file_uri('assets/src/images/hero-section-img.png'); ?>" class="d-block w-100"
-                alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="<?php echo get_theme_file_uri('assets/src/images/hero-section-img.png'); ?>" class="d-block w-100"
-                alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="<?php echo get_theme_file_uri('assets/src/images/hero-section-img.png'); ?>" class="d-block w-100"
-                alt="...">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div> -->
 <?php
 get_footer();
 ?>
